7490665c4b4ea7f9fb275281226a19cc7f321475
MrFacundo <ftroitero@gmail.com>
2024-12-22 13:20:04 +0000

    added all models to admin page

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -2,26 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, GameStats, Friendship
 
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ('email', 'username', 'email_is_verified', 'is_staff', 'is_active')
-    list_filter = ('email_is_verified', 'is_staff', 'is_active')
-    search_fields = ('email', 'username')
-    ordering = ('email',)
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal Info', {'fields': ('username',)}),
-        ('Permissions', {'fields': ('is_staff', 'is_active')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
-    )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'username', 'password1', 'password2', 'is_staff', 'is_active')}
-        ),
-    )
-
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(CustomUser)
+admin.site.register(GameStats)
+admin.site.register(Friendship)
