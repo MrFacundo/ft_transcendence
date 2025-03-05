977b9f00e93d8dbe734387754ea54c7f92948c35
MrFacundo <ftroitero@gmail.com>
2025-03-05 00:20:05 +0000

    added validations on backend post and patch endpoints

--- backend/app/users/serializers.py
+++ backend/app/users/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 from django.conf import settings
 import os
-from rest_framework.exceptions import ValidationError, APIException
-from .models import CustomUser, GameStats, Friendship
+from .models import Friendship
 from django.db.models import Q
 from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth import get_user_model
 from django.core.validators import MaxLengthValidator
+from .services import UserValidationService
 
 User = get_user_model()
 
@@ -20,46 +20,60 @@ class AvatarUploadMixin:
         return representation
 
 class UserSerializer(serializers.ModelSerializer, AvatarUploadMixin):
-    password = serializers.CharField(write_only=True, validators=[validate_password, MaxLengthValidator(20)])
-    new_password = serializers.CharField(write_only=True, required=False, validators=[validate_password, MaxLengthValidator(20)])
+    password = serializers.CharField(
+        write_only=True, 
+        validators=[validate_password, MaxLengthValidator(20)]
+    )
+    new_password = serializers.CharField(
+        write_only=True, 
+        required=False, 
+        validators=[validate_password, MaxLengthValidator(20)]
+    )
     game_stats = serializers.SerializerMethodField()
     friendship_status = serializers.SerializerMethodField()
 
-    # FIXME: This is a workaround to avoid circular import
+    class Meta:
+        model = User
+        fields = [
+            'id', 'username', 'email', 'password', 'avatar_oauth', 
+            'avatar_upload', 'two_factor_method', 'new_email', 
+            'new_password', 'date_joined', 'game_stats', 'friendship_status'
+        ]
+        read_only_fields = ['avatar_oauth', 'date_joined', 'game_stats']
+
+    def validate(self, attrs):
+        if not self.instance:
+            UserValidationService.validate_creation_fields(attrs)
+        else:
+            UserValidationService.validate_update_fields(attrs)
+        return attrs
+
+    def validate_email(self, value):
+        return UserValidationService.validate_email_uniqueness(value, self.instance)
+
+    def validate_avatar_upload(self, value):
+        return UserValidationService.validate_avatar_size(value)
+
+    def validate_two_factor_method(self, value):
+        return UserValidationService.validate_two_factor_method(value)
+
     def get_game_stats(self, obj):
         from app.games.serializers import GameStatsSerializer
         return GameStatsSerializer(obj.game_stats).data if obj.game_stats else None
 
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password', 'avatar_oauth', 'avatar_upload', 'two_factor_method', 'new_email', 'new_password', 'date_joined', 'game_stats', 'friendship_status']
-        read_only_fields = ['id', 'avatar_oauth', 'date_joined', 'game_stats']
-
-    def validate(self, attrs):
-        if not self.instance:
-            allowed_fields = ['username', 'email', 'password']
-            invalid_fields = [field for field in attrs if field not in allowed_fields]
-            if invalid_fields:
-                raise serializers.ValidationError(f"Fields {', '.join(invalid_fields)} are not allowed during user creation.")
-        return attrs
-        
     def get_friendship_status(self, obj):
         request = self.context.get('request', None)
-        if request and request.user.is_authenticated:
-            friendship = Friendship.objects.filter(
-                (Q(sender=request.user) & Q(receiver=obj)) | 
-                (Q(sender=obj) & Q(sender=request.user))
-            ).first()
-            if friendship:
-                return friendship.status
-        return None
-    
-    def update(self, instance, validated_data):
-        new_password = validated_data.pop('new_password', None)
-        if new_password:
-            instance.set_password(new_password)
-        return super().update(instance, validated_data)
+        if not request or not request.user.is_authenticated:
+            return None
+        user_id = self.context.get('view').kwargs.get('pk')
+        if not user_id or int(user_id) == request.user.id:
+            return None
+        friendship = Friendship.objects.filter(
+            (Q(sender=request.user) & Q(receiver=obj)) |
+            (Q(sender=obj) & Q(receiver=request.user))
+        ).first()
+
+        return friendship.status if friendship else None
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
@@ -68,22 +82,13 @@ class UserSerializer(serializers.ModelSerializer, AvatarUploadMixin):
             user.set_password(password)
             user.save()
         return user
-    
-    def validate_new_email(self, value):
-        if User.objects.filter(email=value).exists() or User.objects.filter(new_email=value).exists():
-            raise serializers.ValidationError("This email is already registered to another account.")
-        return value
 
-    def validate_avatar_upload(self, value):
-        if value.size > 5 * 1024 * 1024:
-            raise ValidationError("File too large. Size should not exceed 5 MB.")
-        return value
-    
-    def validate_two_factor_method(self, value):
-        if value == 'authenticator':
-            raise serializers.ValidationError("Authenticator 2FA method can't be set through this view.")
-        return value
-    
+    def update(self, instance, validated_data):
+        new_password = validated_data.pop('new_password', None)
+        if new_password:
+            instance.set_password(new_password)
+        return super().update(instance, validated_data)
+
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         return self.add_avatar_upload_path(representation, instance)
