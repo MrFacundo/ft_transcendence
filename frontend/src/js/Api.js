97455dd8c4b5a606a1a483baccbd976a48f4f798
Turman17 <70319205+turman17@users.noreply.github.com>
2025-03-09 19:10:08 +0200

    feat: refactored ai-bot

--- frontend/src/js/Api.js
+++ frontend/src/js/Api.js
@@ -300,6 +300,10 @@ export class Api {
         });
     }
 
+    async createAIGame(params) {
+    return this.request("post", "/games/ai/", params);
+  }
+
     /**
      * Fetches the avatar image as a blob object URL.
      * @param {string} path - The path to the avatar image.
