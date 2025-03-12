635f71d699c2023c52ed7f86f1719ca992e287a0
MrFacundo <ftroitero@gmail.com>
2025-03-12 17:13:38 +0000

    added missing files

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -57,7 +57,7 @@ module.exports = (env, argv) => {
     module: {
       rules: [
         {
-          test: /\.(png|jpe?g|gif|ico|svg|webp)$/,
+          test: /\.(png|jpe?g|gif|svg|webp)$/,
           type: "asset/resource",
           generator: {
             filename: "static/images/[name]" + (isProduction ? ".[contenthash]" : "") + "[ext]",
