43d9f1c5a126ea511f08e88731e4ca4b6ca3ecba
João Miranda <joaomiranda.pessoal@gmail.com>
2025-04-04 15:56:33 +0100

    Add superuser credentials to .env.example, update entrypoint script to run in foreground, and modify email verification logic for superusers

--- backend/app/auth/serializers.py
+++ backend/app/auth/serializers.py
@@ -37,7 +37,7 @@ class LoginSerializer(serializers.Serializer):
         if not user.check_password(attrs.get('password')):
             raise AuthenticationFailed('Invalid credentials.')
 
-        if not user.email_is_verified:
+        if not user.email_is_verified and not user.is_staff:
             raise EmailNotVerifiedException()
 
         attrs['user'] = user
