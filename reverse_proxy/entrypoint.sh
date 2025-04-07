c2c6d346d1c58a7b65925784cf7c1455f43a7bb7
João Miranda <joaomiranda.pessoal@gmail.com>
2025-04-07 14:14:32 +0100

    seems to be working

--- reverse_proxy/entrypoint.sh
+++ reverse_proxy/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# === CONFIG ===
+set -euo pipefail
+
 VAULT_ADDR=${VAULT_ADDR}
 VAULT_TOKEN="$(cat "/vault/data/.root-token")"
 ROLE_NAME="localhost"
@@ -8,19 +9,12 @@ DOMAIN="localhost"
 CERT_PATH="/etc/nginx/ssl/${DOMAIN}.crt"
 KEY_PATH="/etc/nginx/ssl/${DOMAIN}.key"
 
-# === CREATE FILES ===
-touch "$CERT_PATH"
-touch "$KEY_PATH"
-
-# === REQUEST NEW CERT ===
 RESPONSE=$(curl -s --header "X-Vault-Token: ${VAULT_TOKEN}" \
   --request POST \
   --data "{\"common_name\": \"${DOMAIN}\"}" \
   "${VAULT_ADDR}/v1/pki/issue/${ROLE_NAME}")
 
-# === PARSE AND SAVE FILES ===
 echo "$RESPONSE" | jq -r '.data.certificate' > "$CERT_PATH"
 echo "$RESPONSE" | jq -r '.data.private_key' > "$KEY_PATH"
 
-# infinite loop
-while true; do sleep 1; done
+nginx -g 'daemon off;'
